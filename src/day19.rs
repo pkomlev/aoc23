715df4373465f23329119b7c147160c6c4b9e645
pkomlev <komlev@gmail.com>
2023-12-30 13:22:06 -0800

    day 19 part 2

--- src/day19.rs
+++ src/day19.rs
@@ -6,7 +6,6 @@ use crate::util;
 
 static M: Lazy<HashMap<&str, usize>> = Lazy::new(|| {
     let mut ret = HashMap::new();
-    ret.insert(",", ret.len());
     ret.insert("x", ret.len());
     ret.insert("m", ret.len());
     ret.insert("a", ret.len());
@@ -21,25 +20,25 @@ struct Rule {
     dst: String,
 }
 
+struct RulesSet {
+    rules: Vec<Rule>,
+    fallback: String,
+}
+
 static RE_INPUT_1: Lazy<Regex> = Lazy::new(|| Regex::new(r"\{(.*)\}").unwrap());
 static RE_INPUT_2: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\w)=(\d+)").unwrap());
 static RE_RULE_1: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\w+)\{(.*)\}").unwrap());
 static RE_RULE_2: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\w)([<>])(\d+):(\w+)").unwrap());
 
-fn parse_rule(repr: &str) -> Rule {
+fn parse_rule(repr: &str, ruleset: &mut RulesSet) {
     match RE_RULE_2.captures(repr) {
-        Some(captures) => Rule {
+        Some(captures) => ruleset.rules.push(Rule {
             idx: M[captures.get(1).unwrap().as_str()],
             gt: captures.get(2).unwrap().as_str() == ">",
             val: captures.get(3).unwrap().as_str().parse().unwrap(),
             dst: captures.get(4).unwrap().as_str().to_owned(),
-        },
-        None => Rule {
-            idx: 0,
-            gt: true,
-            val: i32::min_value(),
-            dst: repr.to_owned(),
-        },
+        }),
+        None => ruleset.fallback = repr.to_owned(),
     }
 }
 
@@ -52,9 +51,62 @@ fn rule_matches(rule: &Rule, input: &Vec<i32>) -> bool {
     }
 }
 
-pub fn run(filename: &str, _: bool) {
+fn count(rules: &HashMap<String, RulesSet>, curr: &str, bounds: &Vec<(i32, i32)>) -> i64 {
+    if curr == "R" {
+        return 0;
+    }
+
+    if curr == "A" {
+        let mut mul: i64 = 1;
+        for range in bounds {
+            let d: i64 = (range.1 - range.0 + 1).into();
+            mul *= d;
+        }
+
+        return mul;
+    }
+
+    let mut work = bounds.clone();
+    let mut sum = 0;
+
+    let ruleset = &rules[curr];
+    for rule in &ruleset.rules {
+        // case when rule matches.
+        let saved = work[rule.idx];
+        if rule.gt {
+            let lower = (rule.val + 1).max(work[rule.idx].0);
+            if lower <= work[rule.idx].1 {
+                work[rule.idx].0 = lower;
+                sum += count(rules, &rule.dst, &work);
+            }
+        } else {
+            let upper = (rule.val - 1).min(work[rule.idx].1);
+            if upper >= work[rule.idx].0 {
+                work[rule.idx].1 = upper;
+                sum += count(rules, &rule.dst, &work);
+            }
+        }
+
+        // negate and continue.
+        work[rule.idx] = saved;
+        if rule.gt {
+            work[rule.idx].1 = rule.val;
+        } else {
+            work[rule.idx].0 = rule.val;
+        }
+
+        // if interval becomes invalid - done here.
+        if work[rule.idx].0 > work[rule.idx].1 {
+            return sum;
+        }
+    }
+
+    sum + count(rules, &ruleset.fallback, &work)
+}
+
+pub fn run(filename: &str, adv: bool) {
     let mut new_line = false;
-    let mut rules: HashMap<String, Vec<Rule>> = HashMap::new();
+    let mut rules: HashMap<String, RulesSet> = HashMap::new();
     let mut inputs: Vec<Vec<i32>> = Vec::new();
     if let Ok(lines) = util::read_lines(filename) {
         for (_, line) in lines.enumerate() {
@@ -84,10 +136,12 @@ pub fn run(filename: &str, _: bool) {
                             .map(|x: regex::Match<'_>| x.as_str())
                             .unwrap();
 
-                        let mut ruleset = Vec::new();
+                        let mut ruleset = RulesSet {
+                            rules: Vec::new(),
+                            fallback: "R".to_owned(),
+                        };
                         for repr in workflow.split(',') {
-                            let rule = parse_rule(repr);
-                            ruleset.push(rule);
+                            parse_rule(repr, &mut ruleset);
                         }
 
                         rules.insert(node.to_owned(), ruleset);
@@ -97,23 +151,35 @@ pub fn run(filename: &str, _: bool) {
         }
     }
 
-    let mut ret = 0;
-    for input in inputs {
-        let mut curr = "in";
-        while curr != "A" && curr != "R" {
-            let ruleset = &rules[curr];
-            for rule in ruleset {
-                if rule_matches(&rule, &input) {
-                    curr = &rule.dst;
-                    break;
+    if adv {
+        let mut bounds = Vec::new();
+        bounds.resize(M.len(), (1, 4000));
+        let sum = count(&rules, "in", &bounds);
+        println!("{}", sum);
+    } else {
+        let mut ret = 0;
+        for input in inputs {
+            let mut curr = "in";
+            while curr != "A" && curr != "R" {
+                let ruleset = &rules[curr];
+                let prev = curr;
+                for rule in &ruleset.rules {
+                    if rule_matches(&rule, &input) {
+                        curr = &rule.dst;
+                        break;
+                    }
                 }
+
+                if prev == curr {
+                    curr = &ruleset.fallback;
+                }
+            }
+
+            if curr == "A" {
+                ret += input.iter().sum::<i32>();
             }
         }
 
-        if curr == "A" {
-            ret += input.iter().sum::<i32>();
-        }
+        println!("{}", ret);
     }
-
-    println!("{}", ret);
 }
